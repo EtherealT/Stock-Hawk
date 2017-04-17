c040284ea3e5eafa837f81455fae91434a6c461f
Tobi Adeyinka <tobex9434@yahoo.com>
2017-04-18 01:09:50 +0200

    add line chart (without actual data for now) to stock details activity

--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailsActivity.java
@@ -1,10 +1,10 @@
 package com.udacity.stockhawk.ui;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.udacity.stockhawk.R;
 
 public class StockDetailsActivity extends AppCompatActivity{
@@ -13,5 +13,9 @@ public class StockDetailsActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_details);
+
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
     }
+
 }
