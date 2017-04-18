d5adaa0de46624f46cbd58712af66976908f9d07
Tobi Adeyinka <tobex9434@yahoo.com>
2017-04-18 21:43:32 +0200

    implement stock history request for stock detail activity graph

--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailsActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailsActivity.java
@@ -2,6 +2,7 @@ package com.udacity.stockhawk.ui;
 
 import android.util.Log;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import com.udacity.stockhawk.R;
 import com.github.mikephil.charting.charts.LineChart;
 
+import java.util.List;
+import java.io.IOException;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.Interval;
+import yahoofinance.histquotes.HistoricalQuote;
+
 public class StockDetailsActivity extends AppCompatActivity{
 
     private String stockSymbol;
+    private Stock stock;
+    private List<HistoricalQuote> history;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,10 +31,36 @@ public class StockDetailsActivity extends AppCompatActivity{
 
         Intent intent = getIntent();
         stockSymbol = intent.getStringExtra("stockSymbol");
-        Log.i(getClass() + "stockSymbol", stockSymbol);
+        Log.i(getClass() + ":stockSymbol", stockSymbol);
+
+        new StockDetailsQuery().execute(stockSymbol);
 
         LineChart chart = (LineChart) findViewById(R.id.chart);
+    }
 
+    private class StockDetailsQuery extends AsyncTask<String, Void, Stock>{
+
+        @Override
+        protected Stock doInBackground(String... params) {
+            String stockSymbol = params[0];
+            Stock s = null;
+
+            try {
+                s = YahooFinance.get(stockSymbol, true);
+                history = s.getHistory(Interval.DAILY);
+                Log.i(getClass() + ":stockHistoryDepth", String.valueOf(history.size()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return s;
+        }
+
+        @Override
+        protected void onPostExecute(Stock s) {
+            stock = s;
+            Log.i(getClass() + ":stock", stock.toString());
+       }
     }
 
 }
