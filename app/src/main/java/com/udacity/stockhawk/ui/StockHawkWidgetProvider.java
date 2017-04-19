23c8c13c03458e43478a34bc35918a403427eace
Tobi Adeyinka <tobex9434@yahoo.com>
2017-04-19 14:18:28 +0200

    create widget provider class

---
+++ app/src/main/java/com/udacity/stockhawk/ui/StockHawkWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.udacity.stockhawk.ui;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by tobi on 2017. 04. 19..
+ */
+
+public class StockHawkWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
