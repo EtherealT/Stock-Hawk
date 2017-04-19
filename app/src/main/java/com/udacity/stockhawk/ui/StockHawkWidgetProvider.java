7f62e7a11ff024606362a4a17a607e790878c5ec
Tobi Adeyinka <tobex9434@yahoo.com>
2017-04-19 15:31:18 +0200

    implement widget placeholder

--- app/src/main/java/com/udacity/stockhawk/ui/StockHawkWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockHawkWidgetProvider.java
@@ -1,8 +1,13 @@
 package com.udacity.stockhawk.ui;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.udacity.stockhawk.R;
 
 /**
  * Created by tobi on 2017. 04. 19..
@@ -11,6 +16,13 @@ import android.content.Context;
 public class StockHawkWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
+            RemoteViews rvs = new RemoteViews(context.getPackageName(), R.layout.stock_hawk_widget);
+            appWidgetManager.updateAppWidget(appWidgetId, rvs);
+        }
+
     }
 }
